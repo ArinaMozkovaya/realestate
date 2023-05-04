9660338f1599a236567009b0e54341cc29ecb422
Arina <rimozkovaya@gmail.com>
2023-05-04 20:29:37 +0300

    finish main-block

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ $(document).ready(function() {
 	}); 
 });
 
+const swiper = new Swiper('.slider-main-block', {
+	// Optional parameters
+	loop: true,
+	// Navigation arrows
+	navigation: {
+	  nextEl: '.body-main-block__arrow.swiper-button-next',
+	  prevEl: '.body-main-block__arrow.swiper-button-prev',
+	},
+ });
