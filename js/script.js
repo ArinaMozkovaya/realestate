1bc1c043983136913e1fd77b943a2d7fe4fd6e03
Arina <rimozkovaya@gmail.com>
2023-05-07 20:25:27 +0300

    deals

--- js/script.js
+++ js/script.js
@@ -13,3 +13,16 @@ const swiper = new Swiper('.slider-main-block', {
 	  prevEl: '.body-main-block__arrow.swiper-button-prev',
 	},
  });
+
+ // Табы
+
+ const tabNavItems = document.querySelectorAll('.tabs-deals__button');
+ const tabItems = document.querySelectorAll('.item-tabs');
+ document.addEventListener("click", function (e) {
+	const trargetElement = e.target;
+	if(trargetElement.closest('.tabs-deals__button')){
+		tabNavItems.forEach(( tabNavItems, index ) => {
+
+		});
+	}
+ });
